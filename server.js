4f19c0a8c41dd4cca1bd9b5fbca33fd56e9bc6a0
Steffano <smalagadev@gmail.com>
2019-05-31 23:57:23 -0400

    HTML updates

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 // Declare dependencies, variables and constants
 const express = require('express');
 const path = require('path');
+const favicon = require('serve-favicon');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
+app.use(favicon(__dirname + '/app/public/assets/images/favicon.png'));
 
 require('./app/routing/htmlRoutes.js')(app);
 require('./app/routing/apiRoutes.js')(app);
